b1ec32fc5a8d7081eccb0d08d58de788cd99de37
xinkai <xinkai.chen@qq.com>
2014-03-03 21:50:02 +0800

    add support for thunder/flashget/qqdl scheme support.

--- src/frontend/misc.py
+++ src/frontend/misc.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from threading import Timer
+import base64
 
 def getHumanBytesNumber(byteNum):
     kilo = 1024
@@ -33,3 +34,28 @@ def debounce(wait, instant_first = True):
             debounced.t.start()
         return debounced
     return debouncer
+
+def decodePrivateLink(link):
+    # try to return the real link behind thunder:// flashget:// qqdl://
+    if "\n" in link and \
+        "\t" in link and \
+        "\r" in link:
+        raise Exception("decodePrivateLink Failed. Maybe passed in multiple private links? {}".format(link))
+
+    scheme, *path = link.split("://")
+    assert len(path) == 1, "Invalid private link {}.".format(link)
+
+    path = path[0].encode("utf-8")
+    decoded = base64.urlsafe_b64decode(path).decode("utf-8")
+
+    scheme = scheme.lower()
+    if scheme == "thunder":
+        if decoded.startswith("AA") and decoded.endswith("ZZ"):
+            return decoded[2:-2]
+    elif scheme == "flashget":
+        if decoded.startswith("[FLASHGET]") and decoded.endswith("[FLASHGET]"):
+            return decoded[10:-10]
+    elif scheme == "qqdl":
+        return decoded
+    else:
+        raise Exception("Cannot decode private link {}.".format(link))
