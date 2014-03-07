85a69515e1ec3e7f56914d691debcf057cde7a22
xinkai <xinkai.chen@qq.com>
2014-03-08 03:47:51 +0800

    about dialog: shows detail library versions

--- src/frontend/about.py
+++ src/frontend/about.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+XWARE_VERSION = "1.0.10"
+
 from PyQt5.QtWidgets import QDialog
 from ui_about import Ui_dlg_about
 
@@ -7,4 +9,23 @@ class AboutDialog(QDialog, Ui_dlg_about):
     def __init__(self, parent):
         super().__init__(parent)
         self.setupUi(self)
+        self.fillLibVersions()
 
+    def fillLibVersions(self):
+        self.label_xwareVer.setText(XWARE_VERSION)
+
+        from PyQt5.Qt import QT_VERSION, QT_VERSION_STR, PYQT_VERSION, PYQT_VERSION_STR
+        self.label_qtVer.setText("{} ({})".format(QT_VERSION_STR, QT_VERSION))
+        self.label_pyqtVer.setText("{} ({})".format(PYQT_VERSION_STR, PYQT_VERSION))
+
+        import sys
+        self.label_pythonVer.setText(".".join(map(str, sys.version_info)))
+
+        import requests
+        self.label_requestsVer.setText(requests.__version__)
+
+        import pyinotify
+        self.label_pyinotifyVer.setText(pyinotify.__version__)
+
+        from PyQt5 import QtWebKit
+        self.label_qtwebkitVer.setText(QtWebKit.qWebKitVersion())
