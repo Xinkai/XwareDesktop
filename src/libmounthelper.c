f27c6d5b64b34fac1ac4fcdd802878ea7a7a290e
xinkai <xinkai.chen@qq.com>
2014-03-11 05:09:59 +0800

    downloaded files are now of permission 660 instead of 777.

--- src/libmounthelper.c
+++ src/libmounthelper.c
@@ -4,24 +4,36 @@
 #include <dlfcn.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <limits.h>
 
-const char* PROCMOUNTS = "/proc/mounts";
-const char* XWARE_CONF_PATH = "/opt/xware_desktop/mounts";
+const char* PROC_MOUNTS = "/proc/mounts";
+const char* XWARE_MOUNTS_PATH = "/opt/xware_desktop/mounts";
 
-int xdprint(const char* s) {
+#define XD_PRINTF \
+        int (*printf) (const char *__restrict __format, ...) = dlsym(RTLD_NEXT, "printf");
+
+#define XD_PERROR \
+        void (*perror) (const char *__s) = dlsym(RTLD_NEXT, "perror");
+
+#define XD_SPRINTF \
+        int (*sprintf) (char *__restrict __s, const char *__restrict __format, ...) __THROWNL = dlsym(RTLD_NEXT, "sprintf");
+
+int xdperror(const char* s) {
     size_t size = strlen(s);
-    return write(1, s, size); // avoid using puts/printf, which come from stdio.h.
+    return write(2, s, size); // avoid using puts/printf, which come from stdio.h.
 }
 
 int fopen64(const char* path, const char* mode) {
+    XD_PRINTF;
+
     int (*new_fopen64) (const char* path, const char* mode);
     int result;
 
     new_fopen64 = dlsym(RTLD_NEXT, "fopen64");
-    if (strcmp(PROCMOUNTS, path) == 0) {
+    if (strcmp(PROC_MOUNTS, path) == 0) {
         // feed xware a fake instead of the real /proc/mounts
-        xdprint("\nXware Desktop: FOPEN64 on /proc/mounts\n");
-        result = new_fopen64(XWARE_CONF_PATH, mode);
+        printf("\nXware Desktop: FOPEN64 on /proc/mounts");
+        result = new_fopen64(XWARE_MOUNTS_PATH, mode);
     } else {
         // pass on this API call
         result = new_fopen64(path, mode);
@@ -31,10 +43,71 @@ int fopen64(const char* path, const char* mode) {
 }
 
 mode_t umask(mode_t mask) {
+    XD_PRINTF;
+
     mode_t (*new_umask) (mode_t mask);
     mode_t result;
-    xdprint("\nXware Desktop: UMASK to 006\n");
+    printf("Xware Desktop: UMASK %o to 006\n", mask);
     new_umask = dlsym(RTLD_NEXT, "umask");
     result = new_umask(006);
+    return result;
+}
+
+int fchmod(const int fd, mode_t mode) {
+    XD_PRINTF;
+    XD_PERROR;
+    XD_SPRINTF;
+
+    int (*new_fchmod) (const int fd, mode_t mode);
+    int result;
+
+    new_fchmod = dlsym(RTLD_NEXT, "fchmod");
+    mode_t new_mode;
+    // find out what this fd points to
+    struct stat fdstat;
+
+    if (fstat(fd, &fdstat)) {
+        new_mode = mode;
+        perror("Xware Desktop: fstat inside fchmod");
+        goto end;
+
+    } else {
+        // successfully get fd stat
+        char fd_link_path[PATH_MAX];
+        sprintf(fd_link_path, "/proc/self/fd/%u", fd);
+
+        char fd_link_resolved_path[PATH_MAX] = {0};
+        ssize_t tmp = readlink(fd_link_path, fd_link_resolved_path, PATH_MAX);
+        if (tmp < 0) {
+            new_mode = mode;
+            perror("Xware Desktop: readlink");
+            goto end;
+        }
+
+        if (tmp > PATH_MAX) {
+            new_mode = mode;
+            xdperror("Xware Desktop: readlink size insufficient\n");
+            goto end;
+        }
+
+        if (S_ISREG(fdstat.st_mode)) {
+            new_mode = mode & 0660;
+            printf("Xware Desktop: FCHMOD on file (%o->%o)  %s\n", mode, new_mode, fd_link_resolved_path);
+            goto end;
+
+        } else if (S_ISDIR(fdstat.st_mode)) {
+            new_mode = mode & 0771;
+            printf("Xware Desktop: FCHMOD on dir (%o->%o)  %s\n", mode, new_mode, fd_link_resolved_path);
+            goto end;
+
+        } else {
+            // for non-file, non-dir file-like objects like sockets, don't do anything
+            new_mode = mode;
+            goto end;
+        }
+    }
+
+end:
+    result = new_fchmod(fd, new_mode);
     return result;
 }
