935be8904aeef9e1b9a87b635b15d6da6de1097f
xinkai <xinkai.chen@qq.com>
2014-03-20 22:40:56 +0800

    fix monitorWin: when mainWin minimizes, it doesn't close anymore.

--- src/frontend/MonitorWidget/MonitorWidget.py
+++ src/frontend/MonitorWidget/MonitorWidget.py
@@ -10,7 +10,7 @@ class MonitorWidget(QWidget):
     def __init__(self, parent = None):
         super().__init__(parent)
         self.setWindowFlags(Qt.FramelessWindowHint |
-                            Qt.Tool |
+                            Qt.ToolTip |
                             Qt.WindowStaysOnTopHint)
 
     def mousePressEvent(self, qMouseEvent):
