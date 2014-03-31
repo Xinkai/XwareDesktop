79a30d52ecb52647b520d31bb6fb55d7ed668d25
xinkai <xinkai.chen@qq.com>
2014-03-31 11:55:07 +0800

    on Qt5.0, QComboBox doesn't have currentData function, use compat version.

---
+++ src/frontend/Compat/CompatComboBox.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from PyQt5.QtWidgets import QComboBox
+
+class CompatComboBox(QComboBox):
+    def __init__(self, parent):
+        super().__init__(parent)
+        if not hasattr(self, "currentData"):
+            self.currentData = self._currentData
+
+    def _currentData(self):
+        # introduced in Qt 5.2
+        return self.itemData(self.currentIndex())
