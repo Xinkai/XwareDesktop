d5542c899c4a785db18d290af6dd748a077bb508
xinkai <xinkai.chen@qq.com>
2014-06-04 21:50:49 +0800

    #19: monitorwin can now be toggled from the context menus.

--- src/frontend/contextmenu.py
+++ src/frontend/contextmenu.py
@@ -2,9 +2,37 @@
 
 import logging
 
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QAction
+
 from launcher import app
 from Settings.menu import SettingMenu
 from Settings.QuickSpeedLimit import SpeedLimitingWidgetAction
+
+
+class ToggleMonitorWinAction(QAction):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.setText("显示悬浮窗")
+        self.setCheckable(True)
+        self.triggered[bool].connect(self.settingToggled)
+
+    def defaultWidget(self):
+        # Fake a custom actionwidget
+        # see Settings.menu for more information
+        return self
+
+    def loadSetting(self):
+        self.setChecked(app.settings.getbool("frontend", "showmonitorwindow"))
+
+    def saveSetting(self):
+        pass
+        # handled by self.settingToggled
+
+    @pyqtSlot(bool)
+    def settingToggled(self, checked):
+        app.settings.setbool("frontend", "showmonitorwindow", checked)
+        app.settings.applySettings.emit()
 
 
 class ContextMenu(SettingMenu):
@@ -13,5 +41,8 @@ class ContextMenu(SettingMenu):
 
         self._speedLimitAction = SpeedLimitingWidgetAction(self)
         self.addAction(self._speedLimitAction)
+
+        self._toggleMonitorWinAction = ToggleMonitorWinAction(self)
+        self.addAction(self._toggleMonitorWinAction)
 
         self.addAction(app.mainWin.action_exit)
