8362798a054f7818b379d55005f4c829e0c910a0
xinkai <xinkai.chen@qq.com>
2014-06-19 11:21:30 +0800

    update xware to 1.0.21

--- src/shared/__init__.py
+++ src/shared/__init__.py
@@ -4,7 +4,7 @@ import logging
 
 __version__ = "0.9"
 
-XWARE_VERSION = "1.0.20"
+XWARE_VERSION = "1.0.21"
 
 from collections import namedtuple
 BackendInfo = namedtuple("BackendInfo", ["etmPid", "lcPort", "userId", "peerId"])
