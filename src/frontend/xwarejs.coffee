Xinkai/XwareDesktop

commit d7367907838f906f7997c322c5111480e205234f

xwarejs.js: only trigger setLogined/setOnline with booleans

src/frontend/xwarejs.coffee
@@ -212,8 +212,10 @@ class XwareJS
                 console.log("set logined", v)
         })
 
-        xdpy.slotSetOnline(_online)
-        xdpy.slotSetLogined(_logined)
+        if typeof _online is "boolean"
+            xdpy.slotSetOnline(_online)
+        if typeof _logined is "boolean"
+            xdpy.slotSetLogined(_logined)
 
     slotWaitToBindDeviceObserver: (boundPeerId) ->
         # This method exists because the peerids are loaded by an ajax request,

commit e37a91a91ca6bdb015da77512c9cdaf954dd8a36

fix problem introduced in d7367907838f906f7997c322c5111480e205234f: instead of type boolean, it should be type number

src/frontend/xwarejs.coffee
@@ -212,9 +212,9 @@ class XwareJS
                 console.log("set logined", v)
         })
 
-        if typeof _online is "boolean"
+        if typeof _online is "number"
             xdpy.slotSetOnline(_online)
-        if typeof _logined is "boolean"
+        if typeof _logined is "number"
             xdpy.slotSetLogined(_logined)
 
     slotWaitToBindDeviceObserver: (boundPeerId) ->