49667f21096b0442295064edd8df8b5c313d42cc
xinkai <xinkai.chen@qq.com>
2014-08-04 21:37:57 +0800

    #92: add daemon.py

---
+++ src/frontend/libxware/daemon.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+#  -*- coding: utf-8 -*-
+
+import asyncio
+import json
+from shared import XwaredSocketError
+
+
+class XwaredClient(asyncio.Protocol):
+    def __init__(self):
+        super().__init__()
+        self._data = b''
+
+    def data_received(self, data):
+        self._data += data
+
+    def eof_received(self):
+        data = json.loads(self._data.decode("utf-8"))
+        self.donecb(data)
+
+    @staticmethod
+    def donecb(data):
+        raise NotImplementedError()
+
+
+@asyncio.coroutine
+def callXwared(adapter: "XwareAdapter", method: "str", arguments: "jsonfiable"):
+    loop = asyncio.get_event_loop()
+
+    socketPath = adapter.xwaredSocket
+    cb = getattr(adapter, "_donecb_daemon_" + method, None)
+
+    try:
+        transport, protocol = yield from loop.create_unix_connection(XwaredClient, path = socketPath)
+        if cb:
+            protocol.donecb = cb
+        payload = {
+            "method": method,
+            "arguments": arguments,
+        }
+        payloadBytes = json.dumps(payload).encode("utf-8")
+        transport.write(payloadBytes)
+        transport.write_eof()
+    except ConnectionRefusedError:
+        if cb:
+            cb({
+                "error": XwaredSocketError.CLIENT_CONNECTION_REFUSED
+            })
+    except FileNotFoundError:
+        if cb:
+            cb({
+                "error": XwaredSocketError.CLIENT_SOCKET_NOT_FOUND
+            })
+    except:
+        if cb:
+            cb({
+                "error": XwaredSocketError.CLIENT_UNKNOWN
+            })
