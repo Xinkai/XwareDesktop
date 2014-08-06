Xinkai/XwareDesktop

commit d3b68fe685e3049b4b6ff4bc07d471bba35e1810

#92: notifier now depends on TaskItem instead of etmpy

src/frontend/libxware/item.py
@@ -2,9 +2,9 @@
 
 from launcher import app
 from PyQt5.QtCore import pyqtProperty, pyqtSignal, QObject
-from models.ProxyModel import TaskClass
+from datetime import datetime
 
-from .vanilla import TaskClass as XwareTaskClass
+from models.ProxyModel import TaskClass
 
 _SPEED_SAMPLE_COUNT = 50
 
@@ -131,7 +131,7 @@ class XwareTaskItem(QObject):
         self._initialized = False
         self._adapter = adapter
         self._namespace = self._adapter.namespace
-        self._klass = None
+        self._klass = TaskClass.INVALID
 
         self._id = None
         self._name = None
@@ -230,11 +230,11 @@ class XwareTaskItem(QObject):
     def klass(self):
         # _klass is xware class[0-3],
         # return Xware Desktop task class
-        return 1 << self._klass
-
+        return self._xwareClassToClass(self._klass)
         # Xware Local Control doesn't always return reliable state,
         # needs to use class directly
 
+        # from .vanilla import TaskClass as XwareTaskClass
         # return {
         #     XwareTaskClass.DOWNLOADING: TaskClass.RUNNING,
         #     XwareTaskClass.WAITING: TaskClass.RUNNING,
@@ -250,9 +250,24 @@ class XwareTaskItem(QObject):
         #     XwareTaskClass.ERROR: TaskClass.FAILED,
         # }[self.state]
 
-    def update(self, data, klass):
-        self._klass = klass
+    @klass.setter
+    def klass(self, value):
+        oldClass = self.klass
+        self._klass = value
+        newClass = self._xwareClassToClass(value)
+        if oldClass == newClass:
+            return
 
+        if newClass == TaskClass.COMPLETED:
+            timestamp = datetime.timestamp(datetime.now())
+            if 0 <= timestamp - self.completionTime <= 10:
+                app.taskModel.taskCompleted.emit(self)
+
+    @staticmethod
+    def _xwareClassToClass(klass: int):
+        return klass << 1
+
+    def update(self, data, klass):
         self.speed = data.get("speed")
         self._remainingTime = data.get("remainTime")
         self._state = data.get("state")
@@ -272,4 +287,5 @@ class XwareTaskItem(QObject):
             self._initialized = True
             self.initialized.emit()
 
+        self.klass = klass
         self.updated.emit()

src/frontend/Notify/__init__.py
@@ -3,12 +3,11 @@
 import logging
 from launcher import app
 
-from PyQt5.QtCore import QObject, pyqtSlot, QMetaType, QUrl
+from PyQt5.QtCore import QObject, pyqtSlot, QMetaType, QUrl, Qt
 from PyQt5.QtDBus import QDBusConnection, QDBusInterface, QDBusArgument, QDBusMessage
 from PyQt5.QtGui import QDesktopServices
 from PyQt5.QtMultimedia import QSound
 
-import os
 
 _DBUS_NOTIFY_SERVICE = "org.freedesktop.Notifications"
 _DBUS_NOTIFY_PATH = "/org/freedesktop/Notifications"
@@ -16,12 +15,6 @@ _DBUS_NOTIFY_INTERFACE = "org.freedesktop.Notifications"
 
 
 class Notifier(QObject):
-    _conn = None
-    _interface = None
-    _notifications = None  # a dict of notifyId: taskDict
-    _capabilities = None
-    _completedTasksStat = None
-
     def __init__(self, parent):
         super().__init__(parent)
         self._conn = QDBusConnection("Xware Desktop").sessionBus()
@@ -31,9 +24,8 @@ class Notifier(QObject):
                                          _DBUS_NOTIFY_INTERFACE,
                                          self._conn)
 
-        self._notifications = {}
-        self._completedTasksStat = app.etmpy.completedTasksStat
-        self._completedTasksStat.sigTaskCompleted.connect(self.notifyTask)
+        self._notified = {}  # a dict of notifyId: taskDict
+        app.taskModel.taskCompleted.connect(self.notifyTaskCompleted, Qt.DirectConnection)
 
         self._capabilities = self._getCapabilities()
         if "actions" in self._capabilities:
@@ -50,16 +42,15 @@ class Notifier(QObject):
     def isConnected(self):
         return self._conn.isConnected()
 
-    def notifyTask(self, taskId):
-        task = self._completedTasksStat.getTask(taskId)
+    @pyqtSlot("QObject", result = "void")
+    def notifyTaskCompleted(self, taskItem):
+        if app.settings.getbool("frontend", "notifybysound"):
+            self._qSound_complete.play()
 
-        if task.get("state", None) == 11:  # see definitions in class TaskStatistic.
-            if app.settings.getbool("frontend", "notifybysound"):
-                self._qSound_complete.play()
-            self._dbus_notify(task)
-        else:
-            # TODO: Also notify if errors occur
-            pass
+        if not app.settings.getbool("frontend", "popnotifications"):
+            return
+
+        self._dbus_notifyCompleted(taskItem)
 
     def _getCapabilities(self):
         # get libnotify server caps and remember it.
@@ -72,10 +63,7 @@ class Notifier(QObject):
         else:
             return qdBusMsg.arguments()[0]
 
-    def _dbus_notify(self, task):
-        if not app.settings.getbool("frontend", "popnotifications"):
-            return
-
+    def _dbus_notifyCompleted(self, task: "TaskItem"):
         if "actions" in self._capabilities:
             actions = QDBusArgument(["open", "打开", "openDir", "打开文件夹"], QMetaType.QStringList)
         else:
@@ -87,7 +75,7 @@ class Notifier(QObject):
             QDBusArgument(0, QMetaType.UInt),  # replace_id
             QDBusArgument("xware-desktop", QMetaType.QString),  # app_icon
             QDBusArgument("下载完成", QMetaType.QString),  # summary
-            QDBusArgument(task["name"], QMetaType.QString),  # body
+            QDBusArgument(task.name, QMetaType.QString),  # body
             actions,
             {
                 "category": "transfer.complete",
@@ -100,20 +88,27 @@ class Notifier(QObject):
                                                            qdBusMsg.errorMessage()))
         else:
             # add it to the dict
-            self._notifications[qdBusMsg.arguments()[0]] = task
+            notificationId = qdBusMsg.arguments()[0]
+            self._notified[notificationId] = task.id
 
     @pyqtSlot(QDBusMessage)
     def slotActionInvoked(self, msg):
         notifyId, action = msg.arguments()
-        task = self._notifications.get(notifyId, None)
-        if not task:
+        taskId = self._notified.get(notifyId, None)
+        if not taskId:
             # other applications' notifications
             return
-        name = task["name"]  # filename
-        path = task["path"]  # location
+
+        taskItem = app.taskModel.taskManager.get(taskId, None)
+        if not taskItem:
+            logging.debug("taskItem cannot be found anymore in TaskModel.")
+            return
+
+        fullpath = taskItem.fullpath  # path + name
+        path = taskItem.path  # location
 
         if action == "open":
-            openPath = os.path.join(path, name)
+            openPath = fullpath
         elif action == "openDir":
             openPath = path
         elif action == "default":  # Unity's notify osd always have a default action.
@@ -121,6 +116,5 @@ class Notifier(QObject):
         else:
             raise Exception("Unknown action from slotActionInvoked: {}.".format(action))
 
-        localOpenPath = app.mountsFaker.convertToLocalPath(openPath)
-        qUrl = QUrl.fromLocalFile(localOpenPath)
-        QDesktopServices().openUrl(qUrl)
+        qUrl = QUrl.fromLocalFile(openPath)
+        QDesktopServices.openUrl(qUrl)

src/frontend/etmpy.py
@@ -10,7 +10,6 @@ import requests, json
 from json.decoder import scanner, scanstring
 from requests.exceptions import ConnectionError
 from urllib.parse import unquote
-from datetime import datetime
 
 
 class LocalCtrlNotAvailableError(BaseException):
@@ -37,15 +36,11 @@ class _TaskPollingJsonDecoder(json.JSONDecoder):
 class EtmPy(QObject):
     sigTasksSummaryUpdated = pyqtSignal([bool], [dict])
 
-    runningTasksStat = None
-    completedTasksStat = None
-
     def __init__(self, parent):
         super().__init__(parent)
 
         # task stats
         self.runningTasksStat = RunningTaskStatistic(self)
-        self.completedTasksStat = CompletedTaskStatistic(self)
         self.t = threading.Thread(target = self.pollTasks, daemon = True,
                                   name = "tasks polling")
         self.t.start()
@@ -72,9 +67,6 @@ class EtmPy(QObject):
             resRunning = self._requestPollTasks(0)
             self.runningTasksStat.update(resRunning)
 
-            resCompleted = self._requestPollTasks(1)
-            self.completedTasksStat.update(resCompleted)
-
             # emit summary, it doesn't matter using resRunning or resCompleted
             if resRunning is not None:
                 self.sigTasksSummaryUpdated[dict].emit(resRunning)
@@ -84,29 +76,11 @@ class EtmPy(QObject):
 
 
 class TaskStatistic(QObject):
-    _tasks = None  # copy from _stat_mod upon it's done.
-    _tasks_mod = None  # make changes to this one.
-
-    TASK_STATES = {
-        0: ("dload", "下载中"),
-        8: ("wait", "等待中"),
-        9: ("pause", "已停止"),
-        10: ("pause", "已暂停"),
-        11: ("finish", "已完成"),
-        12: ("delete", "下载失败"),
-        13: ("finish", "上传中"),
-        14: ("wait", "提交中"),
-        15: ("delete", "已删除"),
-        16: ("delete", "已移至回收站"),
-        37: ("wait", "已挂起"),
-        38: ("delete", "发生错误"),
-    }
-    _initialized = False  # when the application starts up, it shouldn't fire.
-
     def __init__(self, parent):
         super().__init__(parent)
-        self._tasks = {}
-        self._tasks_mod = {}
+        self._tasks = {}  # copy from _stat_mod upon it's done.
+        self._tasks_mod = {}  # make changes to this one.
+        self._initialized = False  # when the application starts up, it shouldn't fire.
 
     def getTIDs(self):
         tids = list(self._tasks.keys())
@@ -121,41 +95,6 @@ class TaskStatistic(QObject):
 
     def getTasks(self):
         return self._tasks.copy()
-
-
-class CompletedTaskStatistic(TaskStatistic):
-    sigTaskCompleted = pyqtSignal(int)
-
-    def __init__(self, parent = None):
-        super().__init__(parent)
-
-    def update(self, data):
-        if data is None:
-            return
-
-        # make a list of id of recent finished tasks
-        completed = []
-
-        self._tasks_mod.clear()
-        for task in data["tasks"]:
-            tid = task["id"]
-            self._tasks_mod[tid] = task
-
-            if tid not in self._tasks:
-                completed.append(tid)
-
-        self._tasks = self._tasks_mod.copy()
-        if self._initialized:
-            # prevent already-completed tasks firing sigTaskCompleted
-            #   when ETM starting later than frontend
-            # by comparing `completeTime` with `timestamp`
-            # threshold: 10 secs
-            timestamp = datetime.timestamp(datetime.now())
-            for completedId in completed:
-                if 0 <= timestamp - self._tasks[completedId]["completeTime"] <= 10:
-                    self.sigTaskCompleted.emit(completedId)
-        else:
-            self._initialized = True
 
 
 class RunningTaskStatistic(TaskStatistic):

src/frontend/launcher.py
@@ -82,7 +82,7 @@ class XwareDesktop(QApplication):
         # components
         self.etmpy = EtmPy(self)
         self.mountsFaker = mounts.MountsFaker()
-        self.dbusNotify = Notifier(self)
+        self.notifier = Notifier(self)
         self.frontendpy = FrontendPy(self)
         self.scheduler = Scheduler(self)
 

src/frontend/models/TaskModel.py
@@ -23,6 +23,7 @@ class TaskClass(enum.IntEnum):
     COMPLETED = 2
     RECYCLED = 4
     FAILED = 8
+    INVALID = 16
     ALL = RUNNING | COMPLETED | RECYCLED | FAILED
 
 
@@ -31,6 +32,8 @@ class TaskModel(QAbstractTableModel):
     sigAfterInsert = pyqtSignal()
     sigBeforeRemove = pyqtSignal(int)
     sigAfterRemove = pyqtSignal()
+
+    taskCompleted = pyqtSignal("QObject")  # emits from TaskItem
 
     def __init__(self, parent = None):
         super().__init__(parent)

commit a842411d5e37139ab928ab09d4f70ef89232212e

fix regression introduced with d3b68fe685e3049b4b6ff4bc07d471bba35e1810: monitor doesn't work

src/frontend/libxware/item.py
@@ -265,7 +265,7 @@ class XwareTaskItem(QObject):
 
     @staticmethod
     def _xwareClassToClass(klass: int):
-        return klass << 1
+        return 1 << klass
 
     def update(self, data, klass):
         self.speed = data.get("speed")