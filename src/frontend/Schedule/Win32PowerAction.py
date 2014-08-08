45cdac2c88a0cdfa4eb24c68c1a6e58b41f871ec
xinkai <xinkai.chen@qq.com>
2014-08-08 18:17:20 +0800

    fix Win32PowerAction stub signature

--- src/frontend/Schedule/Win32PowerAction.py
+++ src/frontend/Schedule/Win32PowerAction.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
+from PyQt5.QtCore import QObject
 from .model import Action
 
 
-class Win32PowerActionManager(object):
-    def __init__(self):
+class Win32PowerActionManager(QObject):
+    def __init__(self, parent):
+        super().__init__(parent)
         self.actions = [Action.Null]
 
     def act(self, action):
