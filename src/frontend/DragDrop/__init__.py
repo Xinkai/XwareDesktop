3d537d744c082ce973fd21680985ac2a9825555d
xinkai <xinkai.chen@qq.com>
2014-08-24 21:11:52 +0800

    workaround #113.

--- src/frontend/DragDrop/__init__.py
+++ src/frontend/DragDrop/__init__.py
@@ -2,31 +2,40 @@
 
 import logging
 from launcher import app
+from PyQt5.QtCore import QByteArray
+from PyQt5.QtGui import QDragEnterEvent, QDragMoveEvent, QDropEvent
 
 
 class AllowDrop(object):
     def setupDropSupport(self):
         self.setAcceptDrops(True)
 
-    def dropEvent(self, qDropEvent):
-        if qDropEvent.source() is not None:
+    def dropEvent(self, event: QDropEvent):
+        if event.source() is not None:
             return  # drag starts within the application
-        mimeData = qDropEvent.mimeData()
+        mimeData = event.mimeData()
 
         if mimeData.hasUrls():
-            logging.info("drop action: hasUrls.")
-            urls = list(map(lambda qurl: qurl.url(), mimeData.urls()))
+            raw = mimeData.data("text/uri-list")
+            if type(raw) == QByteArray:
+                # Compat. Workaround #113
+                # As of Qt 5.3.1, QMimeData.urls() cannot read UTF-16 encoded QByteArray
+                # the format Chrome passes over.
+                # Use special tactic!
+                url = bytes(raw).decode("UTF-16LE")
+                urls = [url]
+            else:
+                urls = list(map(lambda qurl: qurl.url(), mimeData.urls()))
             app.taskCreationAgent.createTasksAction(urls)
         elif mimeData.hasText():
-            logging.info("drop action: hasText.")
             app.taskCreationAgent.createTasksFromMimeData(mimeData)
         else:
             pass
-        qDropEvent.acceptProposedAction()
+        event.acceptProposedAction()
 
-    def dragMoveEvent(self, dragMoveEvent):
+    def dragMoveEvent(self, event: QDragMoveEvent):
         # even this is a noop, it's required for dropEvent to be called.
         pass
 
-    def dragEnterEvent(self, dragEnterEvent):
-        dragEnterEvent.acceptProposedAction()
+    def dragEnterEvent(self, event: QDragEnterEvent):
+        event.acceptProposedAction()
--- src/frontend/Widgets/MonitorWidget/MonitorWidget.py
+++ src/frontend/Widgets/MonitorWidget/MonitorWidget.py
@@ -10,11 +10,27 @@ from PyQt5.QtWidgets import QWidget
 class MonitorWidget(QWidget):
     def __init__(self, parent = None):
         super().__init__(parent)
+        # self.setAttribute(Qt.WA_X11NetWmWindowTypeDock, True)
+        # self.setAttribute(Qt.WA_X11NetWmWindowTypeSplash, True)
+
+
+
         self._isBeingDragged = False
         self._dragOffset = None
-        self.setWindowFlags(Qt.FramelessWindowHint |
-                            Qt.ToolTip |
-                            Qt.WindowStaysOnTopHint)
+        self.setWindowFlags(Qt.FramelessWindowHint
+                            # Qt.Dialog |
+                            # Qt.ToolTip |
+                            # Qt.Tool |
+                            | Qt.WindowStaysOnTopHint
+                            # | Qt.CustomizeWindowHint
+                            | Qt.X11BypassWindowManagerHint
+                            # Qt.NoDropShadowWindowHint |
+                            # Qt.BypassGraphicsProxyWidget
+        )
+        self.setAttribute(Qt.WA_X11NetWmWindowTypeDock, True)
+                            # Qt.WindowDoesNotAcceptFocus)
+
+        print(hex(int(self.windowFlags())))
 
     def mousePressEvent(self, qMouseEvent):
         if qMouseEvent.button() == Qt.LeftButton:
