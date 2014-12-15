a38baa43fe9e392af34741400c2c1ffd567cd520
xinkai <xinkai.chen@qq.com>
2014-12-15 21:48:29 +0800

    XwareItem: test taskComplete signal is emitted.

---
+++ src/tests_frontend/test_xware_item.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from unittest import mock
+from datetime import datetime
+
+from libxware.item import XwareTaskItem
+from libxware.definitions import TaskClass as XwareTaskClass
+
+
+def _mockLixianVipChannelFactory():
+    result = {
+        "type": 0,
+    }
+    return result
+
+
+def _mockPayloadFactory(**kwargs):
+    result = {
+        "progress": 0,
+        "remainTime": 0,
+        "size": 1024,
+        "vipChannel": _mockLixianVipChannelFactory(),
+        "lixianChannel": _mockLixianVipChannelFactory(),
+        "name": "Whatever",
+        "path": "/what/ever",
+        "url": "http://what/ever",
+    }
+    result.update(**kwargs)
+    return result
+
+
+class XwareItemTest(unittest.TestCase):
+    def test_item_emit_completed(self):
+        adapter = mock.Mock()
+        adapter.namespace = "foo"
+        taskModel = mock.Mock()
+
+        # Initialize
+        i = XwareTaskItem(adapter = adapter, taskModel = taskModel)
+        i.update(_mockPayloadFactory(), xwareKlass = XwareTaskClass.RUNNING)
+        self.assertRaises(AssertionError,
+                          taskModel.taskCompleted.emit.assert_called_once_with, i)
+
+        # Make it complete
+        i.update(_mockPayloadFactory(completeTime = datetime.timestamp(datetime.now()),
+                                     progress = 10000),
+                 xwareKlass = XwareTaskClass.COMPLETED)
+        taskModel.taskCompleted.emit.assert_called_once_with(i)
+
+        # Don't emit long-ago completed
+        i.update(_mockPayloadFactory(completeTime = 1, progress = 10000),
+                 xwareKlass = XwareTaskClass.COMPLETED)
+        taskModel.taskCompleted.emit.assert_called_once_with(i)
