42cf5b6c3c8953c0f4cb4a45dadaf2d1bf7fa423
xinkai <xinkai.chen@qq.com>
2015-01-24 04:19:29 +0800

    attempt to workaround #161

--- src/frontend/Widgets/__init__.py
+++ src/frontend/Widgets/__init__.py
@@ -4,12 +4,16 @@ import os
 
 from PyQt5.QtCore import QCoreApplication
 
+__all__ = ["CompatSystemTrayIcon", "CompatIcon"]
 
 CompatSystemTrayIcon = None
+CompatIcon = None
+
 if os.name == "posix":
     app = QCoreApplication.instance()
     if app.sessionService.serviceExists("org.kde.StatusNotifierWatcher"):
         from ._KdeSystemTrayIcon import KdeSystemTrayIcon as CompatSystemTrayIcon
+        from ._ThemeIcon import ThemeIcon as CompatIcon
 
 if not CompatSystemTrayIcon:
     from PyQt5.QtWidgets import QSystemTrayIcon
@@ -17,3 +21,5 @@ if not CompatSystemTrayIcon:
         CompatSystemTrayIcon = QSystemTrayIcon
     else:
         from ._DummySystemTrayIcon import DummySystemTrayIcon as CompatSystemTrayIcon
+
+    from PyQt5.QtGui import QIcon as CompatIcon
